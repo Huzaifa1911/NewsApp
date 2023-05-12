f81f11f60339d3ee387a8b9660f51980199dc88f
Huzii1911 <huzaifa.arshad@emumba.com>
2023-05-12 15:55:39 +0500

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Huzaifa Arshad on 12/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
